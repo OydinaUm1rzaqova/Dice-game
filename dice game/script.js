f84967209b1f6bfb4d5280ad12e89673c4163fd6
Oydina Umirzaqova <164199919+OydinaUm1rzaqova@users.noreply.github.com>
2024-05-12 00:17:34 +0500

    Update script.js

--- dice game/script.js
+++ dice game/script.js
@@ -70,9 +70,9 @@ btnNew.addEventListener('click', () => {
     document.getElementById('current--1').textContent = 0;
     document.getElementById('score--0').textContent = 0;
     document.getElementById('score--1').textContent = 0;
-    document.querySelector('.player--0').classList.remove('player-winner');
-    document.querySelector('.player--1').classList.remove('player-winner');
-    document.querySelector('.player--1').classList.remove('player-active');
-    document.querySelector('.player--0').classList.add('player-active');
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--active');
+    document.querySelector('.player--0').classList.add('player--active');
 
 })
